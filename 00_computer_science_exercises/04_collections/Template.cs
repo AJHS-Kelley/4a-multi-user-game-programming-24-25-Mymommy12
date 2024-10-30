d79945826c8ac6138e9a194c718051b2b88fc5fc
Mymommy12 <Kjdman816@gmail.com>
2024-10-30 12:37:43 -0400

    Class 04_Collections
    By kenedy Davis

---
+++ 00_computer_science_exercises/04_collections/Template.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using System.Collections
+
+
+class 04_Collections {
+  static void Main() { // Do Not Delete!
+
+  /*
+  In C# an collection is known as an ARRAY.
+  -- Each Item in the array is an element
+  -- Number of elements in the array cannot be changed.
+  -- Contents of each element can change.
+  -- Arrays are ordered, Meaning each item has a fixed position.
+  -- Position of the element is the index
+  -- First element Index is 0.
+  */
+
+  // Declaring and defining an array
+  string[] breakfastfoods = {"Pancakes","bacon","Scrambled Eggs", "Cereal", "Water"};
+  int[] testscores = {"F","F","F","F","F"};
+  double[] GPA = {"0.16","2.7","9001","6.9","6.3"};
+  }
+}
